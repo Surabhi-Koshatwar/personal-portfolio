51a39eae2b71ad70e8bd220f0eba5b53be2a0f47
Surabhi-Koshatwar <surabhikoshatwar@gmail.com>
2023-08-20 20:10:50 +0530

    added icons

--- script.js
+++ script.js
@@ -35,3 +35,12 @@ function start() {
 
     window.addEventListener("scroll", FadeInReveal);
 }
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+});
